cbbeb2d60f02694c333be74cde644716f621da08
kbastalec <kristian.bastalec@gmail.com>
2021-11-01 21:15:45 +0100

    Adding animation

--- script.js
+++ script.js
@@ -23,6 +23,23 @@ let ballX = 250;
 let ballY = 350;
 const ballRadius = 5;
 
+// Speed
+let speedY;
+let speedX;
+let trajectoryX;
+let computerSpeed;
+
+// Change Mobile Settings
+if (isMobile.matches) {
+  speedY = -2;
+  speedX = speedY;
+  computerSpeed = 4;
+} else {
+  speedY = -1;
+  speedX = speedY;
+  computerSpeed = 3;
+}
+
 // Score
 let playerScore = 0;
 let computerScore = 0;
@@ -70,4 +87,117 @@ function createCanvas() {
   renderCanvas();
 }
 
-createCanvas();
+// Adjust Ball Movement
+function ballMove() {
+  // Vertical Speed
+  ballY += -speedY;
+  // Horizontal Speed
+  if (playerMoved && paddleContact) {
+    ballX += speedX;
+  }
+}
+
+// Determine What Ball Bounces Off, Score Points, Reset Ball
+function ballBoundaries() {
+  // Bounce off Left Wall
+  if (ballX < 0 && speedX < 0) {
+    speedX = -speedX;
+  }
+  // Bounce off Right Wall
+  if (ballX > width && speedX > 0) {
+    speedX = -speedX;
+  }
+  // Bounce off player paddle (bottom)
+  if (ballY > height - paddleDiff) {
+    if (ballX > paddleBottomX && ballX < paddleBottomX + paddleWidth) {
+      paddleContact = true;
+      // Add Speed on Hit
+      if (playerMoved) {
+        speedY -= 1;
+        // Max Speed
+        if (speedY < -5) {
+          speedY = -5;
+          computerSpeed = 6;
+        }
+      }
+      speedY = -speedY;
+      trajectoryX = ballX - (paddleBottomX + paddleDiff);
+      speedX = trajectoryX * 0.3;
+    } else if (ballY > height) {
+      // Reset Ball, add to Computer Score
+      ballReset();
+      computerScore++;
+    }
+  }
+  // Bounce off computer paddle (top)
+  if (ballY < paddleDiff) {
+    if (ballX > paddleTopX && ballX < paddleTopX + paddleWidth) {
+      // Add Speed on Hit
+      if (playerMoved) {
+        speedY += 1;
+        // Max Speed
+        if (speedY > 5) {
+          speedY = 5;
+        }
+      }
+      speedY = -speedY;
+    } else if (ballY < 0) {
+      // Reset Ball, add to Player Score
+      ballReset();
+      playerScore++;
+    }
+  }
+}
+
+// Computer Movement
+function computerAI() {
+  if (playerMoved) {
+    if (paddleTopX + paddleDiff < ballX) {
+      paddleTopX += computerSpeed;
+    } else {
+      paddleTopX -= computerSpeed;
+    }
+  }
+}
+
+// Reset Ball to Center
+function ballReset() {
+  ballX = width / 2;
+  ballY = height / 2;
+  speedY = -3;
+  paddleContact = false;
+}
+
+// Called Every Frame
+function animate() {
+  renderCanvas();
+  ballMove();
+  ballBoundaries();
+  computerAI();
+  window.requestAnimationFrame(animate);
+}
+
+// Start Game, Reset Everything
+function startGame() {
+  playerScore = 0;
+  computerScore = 0;
+  ballReset();
+  createCanvas();
+  animate();
+  canvas.addEventListener("mousemove", (e) => {
+    playerMoved = true;
+    // Compensate for canvas being centered
+    paddleBottomX = e.clientX - canvasPosition - paddleDiff;
+    if (paddleBottomX < paddleDiff) {
+      paddleBottomX = 0;
+    }
+    if (paddleBottomX > width - paddleWidth) {
+      paddleBottomX = width - paddleWidth;
+    }
+    // Hide Cursor
+    canvas.style.cursor = "none";
+  });
+}
+
+// On Load
+startGame();
